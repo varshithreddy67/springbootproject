7292c10a7a8c43a5377b9f1d475df4c81710743d
Varshith Krishna Mohan Reddy Tummuru <varshithreddy6776@gmail.com>
2026-07-18 11:42:55 +0530

    Configure H2 database and create Student entity

--- src/main/java/com/varshith/springbootproject/Student.java
+++ src/main/java/com/varshith/springbootproject/Student.java
@@ -1,9 +1,19 @@
 package com.varshith.springbootproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import org.springframework.stereotype.Component;
 
+@Entity
 @Component
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private String name;
     private int age;
@@ -15,6 +25,20 @@ public class Student {
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
