2a442f8de2e84f3ad78ff20d121f4e350bebfa39
Varshith Krishna Mohan Reddy Tummuru <varshithreddy6776@gmail.com>
2026-07-18 10:40:12 +0530

    Implement ApplicationContext

--- src/main/java/com/varshith/springbootproject/SpringbootprojectApplication.java
+++ src/main/java/com/varshith/springbootproject/SpringbootprojectApplication.java
@@ -2,12 +2,22 @@ package com.varshith.springbootproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootprojectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootprojectApplication.class, args);
-    }
 
+        ApplicationContext context =
+                SpringApplication.run(SpringbootprojectApplication.class, args);
+
+        Student student = context.getBean(Student.class);
+
+        student.setName("Varshith Krishna Mohan Reddy Tummuru");
+        student.setAge(21);
+
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Age: " + student.getAge());
+    }
 }
